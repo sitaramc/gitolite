2f2af033f53954e919ca0b9fae94e521fbaf47ba
Sitaram Chamarty <sitaram@sita-lt.atc.tcs.com>
2009-10-03 13:17:02 +0530

    pta-hook.sh: collect delegated config fragments
    collect the delegated config fragments from correspondingly named branches of
    the gitolite-admin repo, and put them all in conf/fragments/
    
    also deprecate changes to conf and keydir locations from now on

--- src/pta-hook.sh
+++ src/pta-hook.sh
@@ -1,6 +1,29 @@
 #!/bin/sh
 
-GIT_WORK_TREE=/home/git/.gitolite git checkout -f
+# get this from your .gitolite.conf; and don't forget this is shell, while
+# that is perl :-)
+export GL_ADMINDIR=/home/git/.gitolite
 
-cd /home/git/.gitolite
+# checkout the master branch to $GL_ADMINDIR
+GIT_WORK_TREE=$GL_ADMINDIR git checkout -f master
+
+# collect all the delegated fragments
+mkdir -p $GL_ADMINDIR/conf/fragments
+for br in $(git for-each-ref --format='%(refname:short)')
+do
+    # skip master (duh!)
+    [[ $br == master ]] && continue
+    # all other branches *should* contain a file called <branchname>.conf
+    # inside conf/fragments; if so copy it
+    if git show $br:conf/fragments/$br.conf > /dev/null 2>&1
+    then
+        git show $br:conf/fragments/$br.conf > $GL_ADMINDIR/conf/fragments/$br.conf
+        echo "(extracted $br conf; `wc -l < $GL_ADMINDIR/conf/fragments/$br.conf` lines)"
+    else
+        echo "                ***** ERROR *****"
+        echo "        branch $br does not contain conf/fragments/$br.conf"
+    fi
+done
+
+cd $GL_ADMINDIR
 src/gl-compile-conf
