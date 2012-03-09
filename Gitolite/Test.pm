dfccf1b0de0e1076125891134d6bf86797ed5f0e
Sitaram Chamarty <sitaram@atc.tcs.com>
2012-03-09 08:29:41 +0530

    t01-basic completed, some change to test.pm as well

--- Gitolite/Test.pm
+++ Gitolite/Test.pm
@@ -27,6 +27,12 @@ use warnings;
 # required preamble for all tests
 try "
     DEF gsh = /TRACE: gsh.SOC=/
+    DEF reject = /hook declined to update/; /remote rejected.*hook declined/; /error: failed to push some refs to/
+
+    DEF AP_1 = cd ../gitolite-admin; ok or die cant find admin repo clone;
+    DEF AP_2 = AP_1; git add conf keydir; ok; git commit -m %1; ok; /master.* %1/
+    DEF ADMIN_PUSH = AP_2 %1; glt push admin origin; ok; gsh; /master -> master/
+
     ./g3-install -c admin
     cd tsh_tempdir;
 ";
