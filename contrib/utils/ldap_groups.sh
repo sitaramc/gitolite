b650b303850f3491e78c9afaa5ff6ee3716501da
Damien Nozay <damien.nozay@gmail.com>
2014-05-06 16:34:40 -0700

    Add sample script for getting group membership from LDAP.

---
+++ contrib/utils/ldap_groups.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Given a username,
+# Provides a space-separated list of groups that the user is a member of.
+#
+# see http://gitolite.com/gitolite/auth.html#ldap
+# GROUPLIST_PGM => /path/to/ldap_groups.sh
+
+ldap_groups() {
+    username=$1;
+    # this relies on openldap / pam_ldap to be configured properly on your
+    # system. my system allows anonymous search.
+    echo $(
+        ldapsearch -x -LLL "(&(objectClass=posixGroup)(memberUid=${username}))" cn \
+        | grep "^cn" \
+        | cut -d' ' -f2
+    );
+}
+
+ldap_groups $@
