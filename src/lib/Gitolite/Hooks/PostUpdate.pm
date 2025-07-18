540a890e2990d174439924e9bf74130fe1c46621
Sitaram Chamarty <sitaramc@gmail.com>
2025-07-18 10:02:21 +0530

    detect HEAD name for admin repo

--- src/lib/Gitolite/Hooks/PostUpdate.pm
+++ src/lib/Gitolite/Hooks/PostUpdate.pm
@@ -20,23 +20,27 @@ use warnings;
 
 sub post_update {
     trace( 3, 'post-up', @ARGV );
-    exit 0 unless grep( m(^refs/heads/master$), @ARGV );
-    # this is the *real* post_update hook for gitolite
 
-    tsh_try("git ls-tree --name-only master");
+    my $master=`git symbolic-ref HEAD`; chomp $master;
+
+    exit 0 unless grep( $_ eq $master, @ARGV );
+    # this is the *real* post_update hook for gitolite
+    $master =~ s(.*/)();
+
+    tsh_try("git ls-tree --name-only $master");
     _die "no files/dirs called 'hooks' or 'logs' are allowed" if tsh_text() =~ /^(hooks|logs)$/m;
 
     my $hooks_changed = 0;
     {
         local $ENV{GIT_WORK_TREE} = $rc{GL_ADMIN_BASE};
 
-        tsh_try("git diff --name-only master");
+        tsh_try("git diff --name-only $master");
         $hooks_changed++ if tsh_text() =~ m(/hooks/common/);
         # the leading slash ensure that this hooks/common directory is below
         # some top level directory, not *at* the top.  That's LOCAL_CODE, and
         # it's actual name could be anything but it doesn't matter to us.
 
-        tsh_try("git checkout -f --quiet master");
+        tsh_try("git checkout -f --quiet $master");
     }
     _system("gitolite compile");
     _system("gitolite setup --hooks-only") if $hooks_changed;
